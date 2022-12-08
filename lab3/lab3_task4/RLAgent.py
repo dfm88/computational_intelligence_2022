964eef224fd039d87ef3310084ce3fc93a020471
dfm <diegof.university@gmail.com>
2022-12-08 09:15:06 +0100

    Reinforcement Learning maze example

---
+++ lab3/lab3_task4/RLAgent.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+ACTIONS = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+
+
+class Agent(object):
+    def __init__(self, states, alpha=0.15, random_factor=0.2):  # 80% explore, 20% exploit
+        self.state_history = [((0, 0), 0)]  # state, reward
+        self.alpha = alpha
+        self.random_factor = random_factor
+        self.G = {}
+        self.init_reward(states)
+
+    def init_reward(self, states):
+        for i, row in enumerate(states):
+            for j, col in enumerate(row):
+                self.G[(j, i)] = np.random.uniform(low=1.0, high=0.1)
+
+    def choose_action(self, state, allowedMoves):
+        maxG = -10e15
+        next_move = None
+        randomN = np.random.random()
+        # EXPLORATION == CHOOSE RANDOM ACTION
+        if randomN < self.random_factor:
+            # if random number below random factor, choose random action
+            next_move = np.random.choice(allowedMoves)
+        # EXPLOITATION == TAKE ACTION WITH GRATER EXPECTED REWARD
+        else:
+            # if exploiting, gather all possible actions and choose one with the highest G (reward)
+            for action in allowedMoves:
+                new_state = tuple([sum(x) for x in zip(state, ACTIONS[action])])
+                if self.G[new_state] >= maxG:
+                    next_move = action
+                    maxG = self.G[new_state]
+
+        return next_move
+
+    def update_state_history(self, state, reward):
+        self.state_history.append((state, reward))
+
+    def learn(self):
+        # all rewards are negatives, the ideal 
+        # reward is 0 that is when the robot 
+        # exits from the maze
+        target = 0
+
+        for prev, reward in reversed(self.state_history):
+            # all previous rewards + alpha * future
+            self.G[prev] = self.G[prev] + self.alpha * (target - self.G[prev])
+            target += reward
+
+        self.state_history = []
+
+        self.random_factor -= 10e-5  # decrease random factor each episode of play
